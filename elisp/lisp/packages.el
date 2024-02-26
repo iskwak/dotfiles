7fa2aa63be3d7152fdc24e365418d8c74cf5c25b
sam kwak <iljung.kwak@gmail.com>
2024-02-26 19:21:05 +0100

    updating

--- elisp/lisp/packages.el
+++ elisp/lisp/packages.el
@@ -4,21 +4,21 @@
 
 (setq my-packages
       '(
-	tramp-container
-	seoul256-theme
-	clang-format
-	dired-sidebar
-	;;dockerfile-mode
-	;;dash
-	;;flycheck-cmake
-	;;cmake-ide
-	;; cmake-compile-commands
-	))
+		tramp-container
+		seoul256-theme
+		clang-format
+		dired-sidebar
+		dockerfile-mode
+		;;dash
+		;;flycheck-cmake
+		;;cmake-ide
+		;; cmake-compile-commands
+		))
 
 ;; refresh package list if it is not already available
 (when (not package-archive-contents) (package-refresh-contents))
 
 ;; install packages from the list that are not yet installed
 (dolist (pkg my-packages)
-    (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
-        (package-install pkg)))
+  (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
+    (package-install pkg)))
